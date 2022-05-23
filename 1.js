4a2e75acc698f6d5ad8f43bf8647724b523c1aa9
rathorevansh <vansh.rathore@habilelabs.io>
2022-05-23 22:22:40 +0530

    problem slolving assignment

---
+++ 1.js
@@ -0,0 +1,24 @@
+// que1
+
+const nums = [3, 0, 1, 2, 4, 5, 6, 7, 9];
+
+
+for (let i = 0; i <= nums.length - 1; i++) {
+const sorted = nums.sort();
+  if (sorted[i] != i) {
+    console.log("missing value is ", i); 
+  }
+}
+
+// que2
+
+var value = [2, 7, 11, 15];
+let target = 9;
+for (let i = 0; i <= value.length; i++) {
+  for (let j = value.length - 1; j >= 0; j--) {
+    if (value[i] + value[j] == target) {
+      console.log("find at position [", i, j, "]");
+      i = value.length; 
+    }
+  }
+}
